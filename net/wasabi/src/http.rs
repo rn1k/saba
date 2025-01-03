d3aa02c51244cdeaebc4614d2b8ef3e7e0b68860
m1k__ <15975983+rn1k@users.noreply.github.com>
2025-01-03 15:16:45 +0900

    [add] httpClientの作成

---
+++ net/wasabi/src/http.rs
@@ -0,0 +1,18 @@
+extern crate alloc;
+use alloc::string::String;
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
+
+pub struct HttpClient {}
+
+impl HttpClient {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        // 仮
+        let response = HttpResponse::new();
+        Ok(response)
+    }
+}
