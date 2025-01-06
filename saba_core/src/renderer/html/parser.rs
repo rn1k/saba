670f051d101ad9ac788dded189ed75e9f38701ca
m1k__ <15975983+rn1k@users.noreply.github.com>
2025-01-07 03:05:50 +0900

    [add] InsertionMode::InHead の実装

--- saba_core/src/renderer/html/parser.rs
+++ saba_core/src/renderer/html/parser.rs
@@ -1,9 +1,11 @@
+use crate::renderer::dom::node::ElementKind;
 use crate::renderer::dom::node::Node;
 use crate::renderer::dom::node::Window;
 use crate::renderer::html::token::HtmlTokenizer;
 use alloc::rc::Rc;
 use alloc::vec::Vec;
 use core::cell::RefCell;
+use core::str::FromStr;
 
 use super::token::HtmlToken;
 
@@ -99,7 +101,56 @@ impl HtmlParser {
                     self.mode = InsertionMode::InHead;
                     continue;
                 }
-                InsertionMode::InHead => {}
+                InsertionMode::InHead => {
+                    match token {
+                        Some(HtmlToken::Char(c)) => {
+                            if c == ' ' || c == '\n' {
+                                self.insert_char(c);
+                                token = self.t.next();
+                                continue;
+                            }
+                        }
+                        Some(HtmlToken::StartTag {
+                            ref tag,
+                            self_closing: _,
+                            ref attributes,
+                        }) => {
+                            if tag == "style" || tag == "script" {
+                                self.insert_element(tag, attributes.to_vec());
+                                self.original_insertion_mode = self.mode;
+                                self.mode = InsertionMode::Text;
+                                token = self.t.next();
+                                continue;
+                            }
+                            // head省略のハンドリング
+                            if tag == "body" {
+                                self.pop_until(ElementKind::Head);
+                                self.mode = InsertionMode::AfterHead;
+
+                                continue;
+                            }
+                            if let Ok(_element_kind) = ElementKind::from_str(tag) {
+                                self.pop_until(ElementKind::Head);
+                                self.mode = InsertionMode::AfterHead;
+                                continue;
+                            }
+                        }
+                        Some(HtmlToken::EndTag { ref tag }) => {
+                            if tag == "head" {
+                                self.mode = InsertionMode::AfterHead;
+                                token = self.t.next();
+                                self.pop_until(ElementKind::Head);
+                                continue;
+                            }
+                        }
+                        Some(HtmlToken::Eof) => {
+                            return self.window.clone();
+                        }
+                    }
+                    // 他のトークンは無視
+                    token = self.t.next();
+                    continue;
+                }
                 InsertionMode::AfterHead => {}
                 InsertionMode::InBody => {}
                 InsertionMode::Text => {}
