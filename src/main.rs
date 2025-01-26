abce9970089c659803fa232d352e700ddb1fe36c
m1k__ <15975983+rn1k@users.noreply.github.com>
2025-01-26 20:06:56 +0900

    [add] アプリケーションの開始時にウィンドウを描画する

--- src/main.rs
+++ src/main.rs
@@ -3,63 +3,27 @@
 
 extern crate alloc;
 
+use alloc::rc::Rc;
 use alloc::string::ToString;
+use core::cell::RefCell;
 use noli::*;
 use saba_core::browser::Browser;
 use saba_core::http::HttpResponse;
 
-static TEST_HTTP_RESPONSE: &str = r#"HTTP/1.1 200 OK
-Data: xx xx xx
-
-<html>
-<head></head>
-<body>
-    <h1 id="title">H1 title</h1>
-    <h2 class="class">H2 title</h2>
-    <p>Test text.</p>
-    <p>
-        <a href="example.com">Link1</a>
-        <a href="example.com">Link2</a>
-    </p>
-</body>
-</html>
-"#;
-
 fn main() -> u64 {
     let browser = Browser::new();
 
-    let response =
-        HttpResponse::new(TEST_HTTP_RESPONSE.to_string()).expect("failed to parse http response");
-    let page = browser.borrow().current_page();
-    page.borrow_mut().receive_response(response);
+    let ui = Rc::new(RefCell::new(WasabiUI::new(browser)));
+
+    match ui.borrow_mut().start() {
+        Ok(_) => {}
+        Err(e) => {
+            println!("failed fails to start: {:?}", e);
+            return 1;
+        }
+    }
 
     0
 }
 
 entry_point!(main);
-
-// ch3
-//
-// #![no_std]
-// #![no_main]
-
-// extern crate alloc;
-
-// use alloc::string::ToString;
-// use net_wasabi::http::HttpClient;
-// use noli::prelude::*;
-
-// fn main() -> u64 {
-//     let client = HttpClient::new();
-//     match client.get("host.test".to_string(), 8000, "/test.html".to_string()) {
-//         Ok(res) => {
-//             println!("response:\n{:#?}", res);
-//         }
-//         Err(e) => {
-//             println!("error:\n{:#?}", e);
-//         }
-//     }
-//     0
-// }
-
-// entry_point!(main);
