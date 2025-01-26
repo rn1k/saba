5b9867a1b10a8b6abb26c61b3836e39766bf9e58
m1k__ <15975983+rn1k@users.noreply.github.com>
2025-01-26 20:01:16 +0900

    [add] ツールバーの描画処理実装

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,1 +1,87 @@
+use alloc::fmt::format;
+use alloc::rc::Rc;
+use core::cell::RefCell;
+use noli::error::OsResult;
+use noli::window::StringSize;
+use noli::window::Window;
+use saba_core::browser::Browser;
+use saba_core::constants::*;
+use saba_core::error::Error;
 
+#[derive(Debug)]
+pub struct WasabiUI {
+    browser: Rc<RefCell<Browser>>,
+    window: Window,
+}
+
+impl WasabiUI {
+    pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
+        Self {
+            browser,
+            window: Window::new(
+                "saba".to_string(),
+                WHITE,
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT,
+            )
+            .unwrap(),
+        }
+    }
+
+    fn setup(&mut self) -> Result<(), Error> {
+        if let Err(error) = self.setup_toolbar() {
+            // OsResutとResultのエラー型が異なるため、エラー型を変換する
+            return Err(Error::InvalidUI(format!(
+                "failed to initialize toolbar with error: {:#?}",
+                error
+            )));
+        }
+
+        self.window.flush();
+        Ok(())
+    }
+
+    fn setup_toolbar(&mut self) -> OsResult<()> {
+        // ツールバーの背景の四角を描画
+        self.window
+            .fill_rect(LIGHTGREY, 0, 0, WINDOW_WIDTH, TOOLBAR_HEIGHT);
+
+        // ツールバーとコンテンツエリアの境目の線を描画
+        self.window
+            .draw_line(GREY, 0, TOOLBAR_HEIGHT, WINDOW_WIDTH - 1, TOOLBAR_HEIGHT)?;
+        self.window.draw_line(
+            DARKGREY,
+            0,
+            TOOLBAR_HEIGHT + 1,
+            WINDOW_WIDTH - 1,
+            TOOLBAR_HEIGHT + 1,
+        )?;
+
+        // アドレスバーの横に"Address"という文字列を描画
+        self.window.draw_string(
+            BLACK,
+            5,
+            5,
+            "Address:",
+            StringSize::Medium,
+            /*underline=*/ false,
+        )?;
+
+        // アドレスバーの四角を描画
+        self.window
+            .fill_rect(WHITE, 70, 2, WINDOW_WIDTH - 74, 2 + ADDRESSBAR_HEIGHT)?;
+
+        // アドレスバーの影の線を描画
+        self.window.draw_line(GREY, 70, 2, WINDOW_WIDTH - 4, 2)?;
+        self.window
+            .draw_line(GREY, 70, 2, 70, 2 + ADDRESSBAR_HEIGHT)?;
+        self.window.draw_line(BLACK, 71, 3, WINDOW_WIDTH - 5, 3);
+
+        self.window
+            .draw_line(GREY, 71, 3, 71, 1 + ADDRESSBAR_HEIGHT)?;
+
+        Ok(())
+    }
+}
